d470ffa9a7b808b0130a4e069ec8b0f25d20df90
IPSITA <121110612+ipsita68@users.noreply.github.com>
2024-06-21 12:45:20 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ export { default as Kanban } from './Kanban';
 export { default as Orders } from './Orders';
 export { default as Employees } from './Employees';
 export { default as Customers } from './Customers';
-export { default as ColorPicker } from './ColorPicker';
 export { default as Calendar } from './Calendar';
 export { default as Area } from './Charts/Area';
 export { default as Bar } from './Charts/Bar';
